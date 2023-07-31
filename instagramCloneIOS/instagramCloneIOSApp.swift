4bd9cc974ba08e5c14a2a1e97d02d3da2c0a3afb
athulthomas <athul@retailcloud.com>
2023-07-31 11:09:21 +0530

    Initial Commit

---
+++ instagramCloneIOS/instagramCloneIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramCloneIOSApp.swift
+//  instagramCloneIOS
+//
+//  Created by Athul Thomas on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct instagramCloneIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
